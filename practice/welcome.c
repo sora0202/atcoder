a62ad676116fb4e16679e0a9f9a6cddb08455ac4
Hiroyuki Imura <sinewave.440@gmail.com>
2020-02-10 13:10:52 +0900

    最初の問題をといた

--- practice/welcome.c
+++ practice/welcome.c
@@ -7,5 +7,10 @@ int main(void)
     int a, b, c;
     char s[STR_MAX_LEN + 1];
 
+    scanf("%d", &a);
+    scanf("%d %d", &b, &c);
+    scanf("%s", s);
+
+    printf("%d %s\n", a + b + c, s);
     return 0;
 }
